34080267023673a03e135e8cbecef31df59e86b2
cahogan <cahogan16@gmail.com>
2024-10-16 20:52:04 -0700

    Add flight information to display

--- core/views/display.py
+++ core/views/display.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
+from core.models import Flight
 
 
 def display(request):
-    context = {}
+    context = {
+        "flights": Flight.objects.all() # TODO: Filter out departures that have long since passed
+    }
     return render(request, "core/display.html", context)
